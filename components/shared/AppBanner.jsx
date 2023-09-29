c5edf7d4e3ef7f10c6ed77e8798e5f6668b7678d
webju <emmanuel.walubengo@strathmore.edu>
2023-09-29 10:54:50 +0300

    first commit

--- components/shared/AppBanner.jsx
+++ components/shared/AppBanner.jsx
@@ -24,7 +24,7 @@ function AppBanner() {
 					}}
 					className="font-general-semibold text-2xl lg:text-3xl xl:text-4xl text-center sm:text-left text-ternary-dark dark:text-primary-light "
 				>
-					Hi, I am Emmanuel Barasa
+					Prix Technologies
 				</motion.h1>
 				<motion.p
 					initial={{ opacity: 0 }}
@@ -36,7 +36,7 @@ function AppBanner() {
 					}}
 					className="font-general-medium mt-4 text-md md:text-xl lg:text-2xl xl:text-3xl text-center sm:text-left leading-normal text-gray-500 dark:text-gray-200"
 				>
-					A Full-Stack Developer & Machine Learning Enthusiast
+					Web & Mobile Development, Machine Learning
 				</motion.p>
 				<motion.div
 					initial={{ opacity: 0 }}
--- components/shared/AppHeader.jsx
+++ components/shared/AppHeader.jsx
@@ -50,11 +50,11 @@ function AppHeader() {
 						<Link href="/">
 							{activeTheme === 'dark' ? (
 								<h1 className="text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2">
-								Emmanuel Barasa
+								Prix Technologies
 							</h1>
 							) : (
 								<h1 className="text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2">
-									Emmanuel Barasa
+									Prix Technologies
 								</h1>
 
 							)}
@@ -112,7 +112,7 @@ function AppHeader() {
 					</div>
 					<div className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark">
 						<Link href="/about" aria-label="About Me">
-							About Me
+							About
 						</Link>
 					</div>
 					<div className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark">
